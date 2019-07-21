c7152862f1816beb84ff623bcd883580b1f030e3
Diego Cezar <diegocezar.comercial@gmail.com>
2019-07-21 16:10:29 -0300

    3 - Adicionando React-hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,10 +15,12 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module'
   },
-  plugins: ['react', 'prettier'],
+  plugins: ['react', 'prettier', 'react-hooks'],
   rules: {
     'prettier/prettier': 'error',
     'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
-    'import/prefer-default-export': 'off'
+    'import/prefer-default-export': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn'
   }
 };

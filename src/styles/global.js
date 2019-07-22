5cf9eb74da1129a2c7e72b981d0d389617fc65ec
Diego Cezar <diegocezar.comercial@gmail.com>
2019-07-22 02:11:17 -0300

    12 - Alterando tamanho do body e fonte

--- src/styles/global.js
+++ src/styles/global.js
@@ -2,6 +2,11 @@ import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
   html, body, #root {
-    min-height: 100vh;
+    min-height: 100%;
+  }
+
+  body {
+    background-color: #0b0a0d;
+    font-family: Comfortaa, sans-serif;
   }
 `;

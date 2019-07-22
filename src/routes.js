6a4b7866e445bca9c19189ec3d266c9f103c7d30
Diego Cezar <diegocezar.comercial@gmail.com>
2019-07-22 19:22:57 -0300

    17 - Adicionando nova rota

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Main from './pages/Main';
+import Home from './pages/Home';
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" exact component={Main} />
+        <Route path="/" exact component={Home} />
+        <Route path="/main" component={Main} />
       </Switch>
     </BrowserRouter>
   );
